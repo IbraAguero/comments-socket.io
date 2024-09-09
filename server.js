be3b94828230653c35ec817ea13404b234b059ba
IbraAguero <ibrahimaguero@gmail.com>
2024-09-09 13:06:30 -0300

    feat: init proyect

---
+++ server.js
@@ -0,0 +1,26 @@
+import { createServer } from "http";
+import { Server as SocketIO } from "socket.io";
+
+const server = createServer();
+const io = new SocketIO(server, { cors: { origin: "*" } });
+
+io.on("connection", (socket) => {
+  console.log("Cliente conectado");
+
+  socket.on("newComment", (data) => {
+    // Cambiado de "comment" a "newComment"
+    console.log("Nuevo comentario:", data);
+    // Aquí puedes emitir a otros clientes
+    socket.broadcast.emit("comment", data); // Para que otros clientes reciban el comentario
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Cliente desconectado");
+  });
+});
+const PORT = process.env.PORT || 4000;
+
+server.listen(PORT, (err) => {
+  if (err) throw err;
+  console.log(`server on port: ${PORT}`);
+});
